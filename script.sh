e03895f42a81711af1e6482d9c1a206a2a7fe0c1
Andrew <andrewneisch@gmail.com>
2020-02-25 13:16:46 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,5 +14,6 @@ while true; do
     mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -t $MQTT_TOPIC -m 1
     echo > output.txt
     ORIG_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
+    sleep $HOLDOFF_TIME
   fi
 done
