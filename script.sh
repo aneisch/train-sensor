a92810ef7db7c3ee10a57110f07a111910670617
Andrew <andrewneisch@gmail.com>
2020-03-12 01:12:15 -0500

    Kill rtl_fm
    Kill rtl_fm for hold off time. No sense in listening if we're just going to throw away the output anyways. Trying to preserve resources.

--- script.sh
+++ script.sh
@@ -3,6 +3,11 @@
 # Monitor changes in count of output file
 # publish to mqtt when detection occurs
 
+# Monitor frequency and output to file
+# Head of train: 452937500
+# End of train: 457937500
+rtl_fm -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a output.txt >> /dev/null &
+
 ORIG_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
 
 while true; do
@@ -11,7 +16,10 @@ while true; do
     sleep 30
   else
     mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -t $MQTT_TOPIC -m 1
+    pkill rtl_fm
     sleep $HOLDOFF_TIME
+    rtl_fm -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a output.txt >> /dev/null &
+    sleep 1
     echo > output.txt
     ORIG_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
   fi
