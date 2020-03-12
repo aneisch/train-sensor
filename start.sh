924b10590fcc57948abdd11d332f1bb5acdd915d
Andrew <andrewneisch@gmail.com>
2020-03-12 01:07:54 -0500

    Remove rtl_fm from start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Monitor frequency and output to file
-# Head of train: 452937500
-# End of train: 457937500
-rtl_fm -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a output.txt >> /dev/null &
-
 /script.sh &
 
 # Make the script stay "running"
