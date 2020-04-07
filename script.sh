16b4c7824969b18f50ce0729e3584d1625cfed40
Andrew <andrewneisch@gmail.com>
2020-04-07 12:01:06 -0500

    commit script.sg

--- script.sh
+++ script.sh
@@ -6,21 +6,21 @@
 # Monitor frequency and output to file
 # Head of train: 452937500
 # End of train: 457937500
-rtl_fm -d $DEVICE_INDEX -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a output.txt >> /dev/null &
+rtl_fm -d $DEVICE_INDEX -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a /tmp/output.txt >> /dev/null &
 
-ORIG_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
+ORIG_SIZE=$(wc -l /tmp/output.txt | cut -f1 -d' ')
 
 while true; do
-  CUR_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
+  CUR_SIZE=$(wc -l /tmp/output.txt | cut -f1 -d' ')
   if [ "$ORIG_SIZE" == "$CUR_SIZE" ]; then
     sleep 30
   else
     mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -t $MQTT_TOPIC -m 1
     pkill rtl_fm
     sleep $HOLDOFF_TIME
-    rtl_fm -d $DEVICE_INDEX -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a output.txt >> /dev/null &
+    rtl_fm -d $DEVICE_INDEX -f $FREQUENCY -M fm -l $SQUELCH -g -4 | xxd | tee -a /tmp/output.txt >> /dev/null &
     sleep 1
-    echo > output.txt
-    ORIG_SIZE=$(wc -l /output.txt | cut -f1 -d' ')
+    echo > /tmp/output.txt
+    ORIG_SIZE=$(wc -l /tmp/output.txt | cut -f1 -d' ')
   fi
 done
